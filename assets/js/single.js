29617299ffdff9323a7bc946d4254970861241f8
amanduke <65379991+amanduke@users.noreply.github.com>
2020-09-13 13:55:01 -0400

    showing issues on the site

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 
+    // declaring a DOM reference
+    var limitWarningEl = document.querySelector("#limit-warning");
+
     var issueContainerEl = document.querySelector("#issues-container");
+
+    var displayWarning = function(repo) {
+        // add text to warning container
+        limitWarningEl.textContent = "To see more than 30 issues, visit";
+   
+        // link element with an href attribute
+        var linkEl= document.createElement("a");
+        linkEl.textContent = "See More Issues on GitHub.com";
+        linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+        linkEl.setAttribute("target", "_blank");
+
+        // append to warning container
+        limitWarningEl.appendChild(linkEl);
+    };
     
     var getReposIssues = function(repo) {
     console.log(repo);
@@ -10,8 +27,12 @@
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                // pass reponse data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    console.log(displayWarning(repo));
+                }
             });
         }
         else {
@@ -34,6 +55,7 @@
         issueEl.setAttribute("target", "_blank");
         // checks to see if function works in the console
         // console.log(issues);
+        
         // create span to hold issue title
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
